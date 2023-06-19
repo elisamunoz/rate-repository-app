e87f04370c7ec53c33b336ad8ca518ad4aa2225d
elisamunoz <elisamunozes@gmail.com>
2023-06-19 12:07:28 +0200

    text components

---
+++ src/components/Text/Text.styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+import theme from "../../styles/themes";
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal
+  },
+  colorTextSecondary: {
+    color: theme.colors.textSecondary
+  },
+  colorPrimary: {
+    color: theme.colors.primary
+  },
+  fontSizeSubheading: {
+    fontSize: theme.fontSizes.subheading
+  },
+  fontWeightBold: {
+    fontWeight: theme.fontWeights.bold
+  }
+})
+
+export default styles;
